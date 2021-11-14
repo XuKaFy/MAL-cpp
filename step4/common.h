6f573afe30f8d3d8e4ee5b5974b4a5663264982d
XuKaFy <36410093+XuKaFy@users.noreply.github.com>
2021-11-14 11:33:24 +0800

    Step 4
    还不支持语法糖(define (x y z) (+ x y z))

---
+++ step4/common.h
@@ -0,0 +1,189 @@
+#ifndef COMMON_H
+#define COMMON_H
+
+#include <string>
+#include <map>
+#include <vector>
+#include <functional>
+
+class AbstractType;
+class NumberType;
+class AtomType;
+class ListType;
+
+enum class Exception {
+    EXP_READ_END,
+    EXP_READ_MATCH_ERROR,
+    EXP_READ_MATCH_MORE,
+    EXP_ANA_NONE,
+    EXP_ANA_MATCHLESS,
+    EXP_ANA_ESCAPE_ERROR,
+    EXP_ENV_NOT_FOUND,
+    EXP_BUILDIN_FUNCTION_ARG_TYPE_ERROR,
+    EXP_BUILDIN_FUNCTION_LENGTH_ERROR,
+    EXP_HELPER_CONVERT_NONE,
+    EXP_HELPER_APPEND_ERROR,
+    EXP_HELPER_GET_ERROR,
+    EXP_EVAL_CANNOT_EXECUTE,
+    EXP_EVAL_BUILDIN_LENGTH_ERROR,
+    EXP_EVAL_BUILDIN_LIST_ERROR,
+    EXP_EVAL_BUILDIN_ARG_ERROR,
+};
+
+enum class Type {
+    TYPE_NUMBER,
+    TYPE_STRING,
+    TYPE_ATOM,
+    TYPE_LIST,
+    TYPE_BUILDIN_FUNCTION,
+    TYPE_LAMBDA,
+    TYPE_NULL,
+};
+
+struct Pair {
+    Pair(AbstractType* first = nullptr, AbstractType* second = nullptr)
+        : first(first), second(second) {}
+    AbstractType *first;
+    AbstractType *second;
+};
+
+typedef     std::string                                 String;
+typedef     double                                      Number;
+typedef     String                                      Atom;
+typedef     Pair                                        List;
+typedef     std::map<String, AbstractType*>             Map;
+typedef     std::function<AbstractType*(ListType*)>     Function;
+
+class AbstractType
+{
+public:
+    virtual Type type() const;
+
+private:
+    Type m_type;
+};
+
+class NumberType : public AbstractType
+{
+public:
+    NumberType(Number n = Number());
+    virtual Type type() const final;
+    Number number() const;
+    void setNumber(Number n);
+
+private:
+    Number m_num;
+};
+
+class AtomType : public AbstractType
+{
+public:
+    AtomType(Atom n = Atom());
+    virtual Type type() const final;
+    Atom atom() const;
+    void setAtom(Atom n);
+
+private:
+    Atom m_atom;
+};
+
+class ListType : public AbstractType
+{
+public:
+    ListType(List n = List());
+    virtual Type type() const final;
+    List list() const;
+    void setList(List n);
+
+private:
+    List m_list;
+};
+
+class StringType : public AbstractType
+{
+public:
+    StringType(String n = String());
+    virtual Type type() const final;
+    Atom string() const;
+    void setString(Atom n);
+
+private:
+    String m_str;
+};
+
+class BuildinFunctionType : public AbstractType
+{
+public:
+    BuildinFunctionType(Function f, String name);
+    virtual Type type() const final;
+
+    AbstractType* process(ListType* obj);
+    String name() const;
+
+private:
+    Function m_f;
+    String m_name;
+};
+
+class LambdaType : public AbstractType
+{
+public:
+    LambdaType(ListType* arg, ListType *body);
+    virtual Type type() const final;
+
+    ListType* arg() const;
+    void setArg(ListType* arg);
+
+    ListType* body() const;
+    void setBody(ListType* body);
+
+private:
+    ListType* m_arg;
+    ListType* m_body;
+};
+
+class Helper
+{
+public:
+    static AbstractType* car(ListType* o);
+    static AbstractType* cdr(ListType* o);
+    static bool atom(AbstractType* o);
+    static bool eq(AbstractType* a1, AbstractType* a2);
+    static ListType* cons(AbstractType* a1, AbstractType* a2);
+
+    static AbstractType* get(ListType *& o);
+    static void next(ListType *& o);
+
+    static bool isEmpty(AbstractType* o);
+    static bool isLast(ListType* o);
+    static bool isList(ListType* o);
+    static bool isSingle(ListType *o);
+    static bool isSelfEvaluating(AbstractType* o);
+    static bool isTrue(AbstractType* o);
+    static bool isFalse(AbstractType* o);
+    static bool isFlat(ListType* o);
+
+    static AbstractType* constantTrue();
+    static AbstractType* constantFalse();
+    
+    static ListType* append(ListType* o, AbstractType* n);
+    static AbstractType* foreach(ListType* o, std::function<void(AbstractType* o)> f);
+
+    template<typename T>
+    static T convert(AbstractType* o, Type t) {
+        if(o == nullptr)
+            throw Exception::EXP_HELPER_CONVERT_NONE;
+        if(o->type() != t)
+            throw Exception::EXP_BUILDIN_FUNCTION_ARG_TYPE_ERROR;
+        return static_cast<T>(o);
+    }
+
+    template<typename T>
+    static T convert(AbstractType* o) {
+        if(o == nullptr)
+            throw Exception::EXP_HELPER_CONVERT_NONE;
+        return static_cast<T>(o);
+    }
+};
+
+#endif // COMMON_H
---
+++ step4/reader.cpp
@@ -0,0 +1,158 @@
+#include "reader.h"
+
+AbstractType* Analyzer::analyze(String s)
+{
+    m_s = s;
+    m_pos = 0;
+    m_len = s.size();
+
+    return elem();
+}
+
+bool Analyzer::remain() const
+{
+    return m_pos < m_len;
+}
+
+void Analyzer::delSpace()
+{
+    while(remain() && isspace(lookahead()))
+        match(lookahead());
+}
+
+AbstractType* Analyzer::number()
+{
+    Number k = 0;
+    bool read = false;
+    while(remain() && isdigit(lookahead())) {
+        k = k * 10 + lookahead() - '0';
+        match(lookahead());
+        read = true;
+    }
+    if(!read)
+        throw Exception::EXP_ANA_NONE;
+    return new NumberType(k);
+}
+
+AbstractType* Analyzer::atom()
+{
+    String s;
+    if(remain() && isAtomHead(lookahead())) {
+        s += lookahead();
+        match(lookahead());
+        while(remain() && isAtomBody(lookahead())) {
+            s += lookahead();
+            match(lookahead());
+        }
+        return new AtomType(s);
+    }
+    throw Exception::EXP_ANA_NONE;
+}
+
+AbstractType* Analyzer::list()
+{
+    match("(");
+    ListType* root = new ListType();
+    ListType* current = root;
+    while(remain() && lookahead() != ')') {
+        current = Helper::append(current, elem());
+        delSpace();
+    }
+    if(Helper::car(current) != nullptr)
+        current->setList(List{Helper::car(current), new ListType()});
+    match(")");
+    return root;
+}
+
+AbstractType* Analyzer::string()
+{
+    String s;
+    match('"');
+    while(remain() && lookahead() != '"') {
+        if(lookahead() == '\\') {
+            s += lookahead();
+            match(lookahead());
+            if(lookahead() == '"') { // \" must be processed in reader
+                s += lookahead();
+                match(lookahead());
+            }
+        } else {
+            s += lookahead();
+            match(lookahead());
+        }
+    }
+    match('"');
+    return new StringType(s);
+}
+
+AbstractType* Analyzer::elem()
+{
+    delSpace();
+    if(!remain())
+        throw Exception::EXP_READ_MATCH_MORE;
+    if(lookahead() == '(') {
+        return list();
+    } else if(isdigit(lookahead())) {
+        return number();
+    } else if(isAtomHead(lookahead())) {
+        return atom();
+    } else if(lookahead() == '"') {
+        return string();
+    } else {
+        throw Exception::EXP_ANA_MATCHLESS;
+    }
+}
+
+bool Analyzer::isSymbol(String::value_type c)
+{
+    if(c == '_') return true;
+    if(c == '+') return true;
+    if(c == '-') return true;
+    if(c == '*') return true;
+    if(c == '/') return true;
+    if(c == '%') return true;
+    if(c == '^') return true;
+    if(c == '<') return true;
+    if(c == '>') return true;
+    if(c == '=') return true;
+    return false;
+}
+
+bool Analyzer::isAtomHead(String::value_type c)
+{
+    return isSymbol(c) || isalpha(c);
+}
+
+bool Analyzer::isAtomBody(String::value_type c)
+{
+    return isalnum(c) || isSymbol(c);
+}
+
+String::value_type Analyzer::lookahead() const
+{
+    if(m_pos == m_len) {
+        throw Exception::EXP_READ_MATCH_MORE;
+    }
+    return m_s[m_pos];
+}
+
+void Analyzer::match(String::value_type k)
+{
+    if(k == lookahead()) {
+        ++m_pos;
+    } else {
+        throw Exception::EXP_READ_MATCH_ERROR;
+    }
+}
+
+void Analyzer::match(String k)
+{
+    for(auto i : k)
+        match(i);
+}
+
+AbstractType* Reader::read_str(String s)
+{
+    Analyzer az;
+    return az.analyze(s);
+}
